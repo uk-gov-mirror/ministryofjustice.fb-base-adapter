9efdee8489e0d4e3164d03fea6d4be7584ebcaf6
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-06-09 12:49:19 +0100

    Add tests for edge cases
    1. When there is no submission
    2. When there is a submission but the encryption key is not correct
    with the payload encrypted.

--- app.rb
+++ app.rb
@@ -6,12 +6,20 @@ require 'jwe'
 
 PAYLOAD_CONTENT_FILE = 'tmp/payload_content.json'
 
-post "/submission" do
+post '/submission' do
   payload = request.body.read
   File.write(PAYLOAD_CONTENT_FILE, payload)
 end
 
-get "/submission" do
+get '/submission' do
   encrypted_payload = File.read(PAYLOAD_CONTENT_FILE)
   JWE.decrypt(encrypted_payload, ENV['ENCRYPTION_KEY'])
+rescue Errno::ENOENT
+  status 404
+  json({ error: 'Submission not found' })
+rescue JWE::InvalidData
+  status 400
+  json(
+    { error: 'Failed to decrypt submission. Is the encryption key correct?' }
+  )
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,20 +3,53 @@ require 'jwe'
 require 'securerandom'
 
 describe '/submission' do
-  let(:encryption_key) { SecureRandom.uuid[0..15] }
   let(:payload) { "{\"foo\": \"bar\"}" }
-  let(:encrypted_payload) { JWE.encrypt(payload, encryption_key, alg: 'dir') }
 
-  context 'when posting submission' do
+  context 'when there is a submission' do
+    let(:encryption_key) { SecureRandom.uuid[0..15] }
     before do
       allow(ENV).to receive(:[]).with('ENCRYPTION_KEY').and_return(encryption_key)
     end
 
-    it 'returns the submission payload' do
-      post '/submission', encrypted_payload
+    context 'when the encryption key is the same as the payload' do
+      let(:encrypted_payload) { JWE.encrypt(payload, encryption_key, alg: 'dir') }
 
+      it 'returns the submission payload' do
+        post '/submission', encrypted_payload
+
+        get '/submission'
+        expect(last_response.body).to eq(payload)
+      end
+    end
+
+    context 'when the encryption key is NOT the same as the payload' do
+      let(:encrypted_payload) { JWE.encrypt(payload, 'a' * 16, alg: 'dir') }
+      let(:error) do
+        JSON.generate(
+          error: 'Failed to decrypt submission. Is the encryption key correct?'
+        )
+      end
+
+      it 'returns the submission payload' do
+        post '/submission', encrypted_payload
+        get '/submission'
+        expect(last_response.status).to be(400)
+        expect(last_response.body).to eq(error)
+      end
+    end
+  end
+
+  context 'when there is no submission' do
+    let(:error) { JSON.generate(error: 'Submission not found') }
+
+    before do
+      File.delete(PAYLOAD_CONTENT_FILE) if File.exist?(PAYLOAD_CONTENT_FILE)
+    end
+
+    it 'returns not found' do
       get '/submission'
-      expect(last_response.body).to eq(payload)
+      expect(last_response.status).to be(404)
+      expect(last_response.body).to eq(error)
     end
   end
 end
