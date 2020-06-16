565af71e7ea87ac34bd7623b38e0f5940ff24d5c
brenetic <brendan.butler@digital.justice.gov.uk>
2020-06-16 13:17:27 +0100

    Add docker compose for adapter
    We are using docker to run tests in CI, so a docker compose is needed

--- deploy/scripts/circle_deploy.sh
+++ deploy/scripts/circle_deploy.sh
@@ -6,7 +6,7 @@ CONFIG_FILE="/tmp/helm_deploy.yaml"
 
 git_sha_tag=$1
 environment_name=$2
-kube_token=$3
+k8s_token=$3
 
 get_secrets() {
     GIT_SSH_COMMAND='ssh -v -i ~/.ssh/id_rsa_7ee744bea14b4e3c56653a94195002cc -o "IdentitiesOnly=yes"' git clone git@github.com:ministryofjustice/formbuilder-base-adapter-deploy.git deploy-config
@@ -15,10 +15,10 @@ get_secrets() {
 }
 
 deploy_with_secrets() {
-    echo -n "$KUBE_CERTIFICATE_AUTHORITY" | base64 -d > .kube_certificate_authority
-    kubectl config set-cluster "$KUBE_CLUSTER" --certificate-authority=".kube_certificate_authority" --server="$KUBE_SERVER"
-    kubectl config set-credentials "circleci_${environment_name}" --token="${kube_token}"
-    kubectl config set-context "circleci_${environment_name}" --cluster="$KUBE_CLUSTER" --user="circleci_${environment_name}" --namespace="formbuilder-base-adapter-${environment_name}"
+    echo -n "$K8S_CLUSTER_CERT" | base64 -d > .kube_certificate_authority
+    kubectl config set-cluster "$K8S_CLUSTER_NAME" --certificate-authority=".kube_certificate_authority" --server="https://api.${K8S_CLUSTER_NAME}"
+    kubectl config set-credentials "circleci_${environment_name}" --token="${k8s_token}"
+    kubectl config set-context "circleci_${environment_name}" --cluster="$K8S_CLUSTER_NAME" --user="circleci_${environment_name}" --namespace="formbuilder-base-adapter-${environment_name}"
     kubectl config use-context "circleci_${environment_name}"
 
     helm template deploy/ \
